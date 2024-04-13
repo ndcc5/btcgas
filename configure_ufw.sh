cddf02cca12fad273400c806343b5bb0722b0914
Yilanjiangchen <ndcc517@gmail.com>
2024-04-13 16:41:53 +0800

    Create configure_ufw.sh

---
+++ configure_ufw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo ufw allow from 119.182.0.0/16
+sudo ufw allow from 119.183.0.0/16
+sudo ufw allow from 119.184.0.0/16
+sudo ufw allow from 119.185.0.0/16
+sudo ufw allow from 119.186.0.0/16
+sudo ufw allow from 119.187.0.0/16
+sudo ufw allow from 119.188.0.0/16
+sudo ufw allow from 119.189.0.0/16
+sudo ufw allow from 112.250.0.0/16
+sudo ufw allow from 222.133.0.0/16
